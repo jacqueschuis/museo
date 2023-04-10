1c09a4f67281eceb1924765efb8d379c5b5796c3
Jack_work <jacquespariseau93@gmail.com>
2023-04-10 16:08:27 -0400

    adding parallax effect to landing

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,1 @@
-const logo = document.querySelector('#logo');
-const logoSpan = document.querySelector('#logoSpan');
-
-logo.addEventListener('click', () => {
-    logoSpan.style.display = 'inline'; 
-})
+const title = document.querySelector('#title');
