046f60f7696597d169af2eaddb902652f8e1fe5e
jacqueschuis <jacquespariseau93@gmail.com>
2023-04-18 07:03:28 -0400

    more show page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,48 +1,47 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 module.exports.registerForm = (req, res) => {
-    res.render('user/register')
-}
+  res.render("user/register");
+};
 
 module.exports.registerUser = async (req, res, next) => {
-    try {
-        const {email, username, password, firstName, lastName } = req.body;
-        const isAdmin = false;
-        const user = new User({email,username,firstName,lastName, isAdmin });
-        const registeredUser = await User.register(user,password);
-        req.login(registeredUser, (err) => {
-            if (err) {
-                return next(err);
-            }
-            req.flash('success', 'welcome to museo');
-            res.redirect('/')
-        });
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
-}
+  try {
+    const { email, username, password, firstName, lastName } = req.body;
+    const isAdmin = false;
+    const user = new User({ email, username, firstName, lastName, isAdmin });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "welcome to museo");
+      res.redirect("/");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/register");
+  }
+};
 
 module.exports.loginForm = (req, res) => {
-    res.render('user/login')
-}
+  res.render("user/login");
+};
 
 module.exports.loginUser = async (req, res) => {
-    req.flash('success', `welcome back, ${req.user.firstName}`);
-    const redirectUrl = req.session.returnTo || '/';
+  req.flash("success", `welcome back, ${req.user.firstName}`);
+  const redirectUrl = req.session.returnTo || "/";
+  delete req.session.returnTo;
+  res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res, next) => {
+  const redirectUrl = req.session.returnTo || "/";
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "you have been signed out");
     delete req.session.returnTo;
     res.redirect(redirectUrl);
-}
-
-module.exports.logout = (req,res,next) => {
-    console.log(req.session)
-    const redirectUrl = req.session.returnTo || '/';
-    req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'you have been signed out')
-        delete req.session.returnTo;
-        res.redirect(redirectUrl);
-    })
-}
+  });
+};
