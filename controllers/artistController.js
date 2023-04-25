5b649922d2f3f5358f1417bee25dd7dc16d4aaf4
Jack_work <jacquespariseau93@gmail.com>
2023-04-25 14:50:04 -0400

    identified all possible combinations of filters for artists, refining the logic and testing each combination

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -20,11 +20,11 @@ module.exports.filterArtists = async (req, res) => {
     const museums = await Museum.find({})
     const {name, filterBornDate, filterDeathDate, filterMuseum} = req.body;
     (name ? queryName = name : queryName = null);
-    (filterBornDate ? queryBornDate = filterBornDate : queryBornDate = -9999);
-    (filterDeathDate ? queryDeathDate = filterDeathDate : queryDeathDate = 9999);
+    (filterBornDate ? queryBornDate = filterBornDate : queryBornDate = -99999);
+    (filterDeathDate ? queryDeathDate = filterDeathDate : queryDeathDate = 99999);
     (filterMuseum ? queryMuseum = filterMuseum : queryMuseum = null);
-if (queryName) {
-  console.log('queryname branch')
+if (queryName && filterMuseum) {
+  console.log('queryname and museum branch')
   const agg = [
     {
       $search: {
@@ -33,7 +33,30 @@ if (queryName) {
           query: queryName,
           path: "name",
           "fuzzy": {
-            "maxEdits": 2,
+            "maxEdits": 1,
+          }
+        }
+      }
+    },
+    {
+      $match: {
+        museums: {$all: [queryMuseum]}
+      }
+    }
+  ]
+  const artists = await Artist.aggregate(agg);
+  return res.render('artist/index', {artists, museums});
+} if (queryName && filterBornDate) {
+  console.log('name birth');
+  const agg = [
+    {
+      $search: {
+        index: "artist-name",
+        autocomplete: {
+          query: queryName,
+          path: "name",
+          "fuzzy": {
+            "maxEdits": 1,
           }
         }
       }
@@ -41,23 +64,92 @@ if (queryName) {
     {
       $match: {
         bornDate: {$gte: parseInt(queryBornDate)},
+    //     deathDate: {$lte: parseInt(queryDeathDate)},
+      }
+    }
+  ]
+  const artists = await Artist.aggregate(agg);
+  return res.render('artist/index', {artists, museums});
+
+} if (queryName && filterDeathDate) {
+  console.log('name death');
+  const agg = [
+    {
+      $search: {
+        index: "artist-name",
+        autocomplete: {
+          query: queryName,
+          path: "name",
+          "fuzzy": {
+            "maxEdits": 1,
+          }
+        }
+      }
+    },
+    {
+      $match: {
+        // bornDate: {$gte: parseInt(queryBornDate)},
         deathDate: {$lte: parseInt(queryDeathDate)},
       }
     }
   ]
-  const artists = await Artist.aggregate(agg)
+  const artists = await Artist.aggregate(agg);
+  return res.render('artist/index', {artists, museums});
+
+} if (queryName) {
+  console.log('name only')
+  const agg = [
+    {
+      $search: {
+        index: "artist-name",
+        autocomplete: {
+          query: queryName,
+          path: "name",
+          "fuzzy": {
+            "maxEdits": 1,
+          }
+        }
+      }
+    },
+    // {
+    //   $match: {
+    //     bornDate: {$gte: parseInt(queryBornDate)},
+    //     deathDate: {$lte: parseInt(queryDeathDate)},
+    //   }
+    // }
+  ]
+  const artists = await Artist.aggregate(agg);
+  return res.render('artist/index', {artists, museums});
+} if (queryMuseum && filterDeathDate) {
+  console.log('museum death');
+  const artists = await Artist.find()
+    .where('museums').all([queryMuseum])
+    .where('deathDate').lte(queryDeathDate);
   return res.render('artist/index', {artists, museums});
 } if (queryMuseum) {
-  console.log('querymuseum branch')
+  console.log('museum only, museum birth')
   const artists = await Artist.find()
     .where('bornDate').gte(queryBornDate)
-    .where('deathDate').lte(queryDeathDate)
     .where('museums').all([queryMuseum]);
   return res.render('artist/index', {artists, museums})
-}
-console.log('reg branch')
+} if (filterBornDate && filterDeathDate) {
+  console.log ('both dates');
   const artists = await Artist.find({})
-    .where('bornDate').gte(parseInt(queryBornDate))
-    .where('deathDate').lte(parseInt(queryDeathDate))
+    .where('deathDate').lte(queryDeathDate)
+    .where('bornDate').gte(queryBornDate);
+  return res.render('artist/index', {artists, museums});
+} if (filterBornDate) {
+  console.log('birth only');
+  const artists = await Artist.find({})
+    .where('bornDate').gte(queryBornDate)
+  return res.render('artist/index', {artists, museums})
+} if (filterDeathDate) {
+  console.log('death only');
+  const artists = await Artist.find({})
+    .where('deathDate').lte(queryDeathDate);
+  return res.render('artist/index', {artists, museums})
+} 
+  console.log('reg branch', queryBornDate, queryDeathDate)
+  const artists = await Artist.find({})
   res.render('artist/index', {artists, museums})
 }
